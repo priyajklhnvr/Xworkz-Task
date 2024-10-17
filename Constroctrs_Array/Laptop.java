32c955a7ddf7e30a7a53082c25fb9151a502891d
PriyaHegde <priyajkl2001@gmail.com>
2024-10-17 08:04:39 +0530

    Array

---
+++ Constroctrs_Array/Laptop.java
@@ -0,0 +1,29 @@
+public class Laptop {
+    String brand;
+    int ram;
+    int storage;
+    String model;
+
+    public Laptop(String brand, int ram, int storage, String model) {
+        this.brand = brand;
+        this.ram = ram;
+        this.storage = storage;
+        this.model = model;
+
+    }
+
+    public void turnOn() {
+
+    }
+
+    public void turnOff() {
+
+    }
+
+    public void laptopDetails() {
+        System.out.println("Laptop Brand: " + brand);
+        System.out.println("Laptop Ram: " + ram);
+        System.out.println("Laptop Storage: " + storage);
+        System.out.println("Laptop Model: " + model);
+    }
+}
