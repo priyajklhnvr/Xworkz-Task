2c77ff9b1c939b81e87fa16ae08adb65f377666d
PriyaHegde <priyajkl2001@gmail.com>
2024-11-26 11:26:30 +0530

    Exception

---
+++ Exception/Laptop.java
@@ -0,0 +1,9 @@
+class Car extends Vehicle {
+    public void start() {
+        System.out.println("Car is starting");
+    }
+
+    public void drive() {
+        System.out.println("Car is being driven");
+    }
+}
